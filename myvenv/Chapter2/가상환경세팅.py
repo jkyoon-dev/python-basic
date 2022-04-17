104391133d16911d66caa60116fd3fdbee9e65f8
jkyoon-dev <jkyoon.dev@gmail.com>
2022-04-18 08:08:34 +0900

    윈도우용 파이썬 가상환경세팅 방법 추가

--- myvenv/Chapter2/가상환경세팅.py
+++ myvenv/Chapter2/가상환경세팅.py
@@ -1,1 +1,20 @@
-print("환경세팅!!")
+# 가상환경을 사용하는 이유
+# A 프로젝트 request 1.0
+# B 프로젝트 request 2.0
+
+# 윈도우용
+
+# venv
+
+# import cmd
+
+
+# 1. 폴더만들기
+# 2. cmd
+# 3. python -m venv myvenv 입력
+# 4. 입력 실행 : myvenv\Scripts\activate
+# 5. 입력 실행 : pip list
+# 6. 입력 실행 : pip install requests    
+# 7. 입력 실행 : pip list
+
+# myvenv 안에 폴더 만들기
